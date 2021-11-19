5f56356f4925e7b19f125b6dc6fc1371b043f293
Anar Enhsaihan <aenhsaihan@gmail.com>
2021-11-18 22:55:05 -0800

    User will ping, bot will pong back

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const Discord = require("discord.js");
 const client = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
 });
+
 const config = require("./config.json");
+const command = require("./command");
 
 client.on("ready", () => {
   console.log("The client is ready!");
+
+  command(client, "ping", (message) => {
+    message.channel.send("Pong!");
+  });
 });
 
 client.login(config.token);
