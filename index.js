22b61b2a0582bd4820e6c80593323897512333ed
Anar Enhsaihan <aenhsaihan@gmail.com>
2021-11-18 22:22:50 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log("The client is ready!");
+});
+
+client.login(config.token);
